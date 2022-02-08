d752274ff0a7c34cace9e42a7bd950cc73e7eb77
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-02-08 14:16:40 +0000

    Refactor code

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,68 +1,20 @@
 import React from 'react';
+import nestedTernary from '../logic/helper';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
+    const operators = '÷x-+=';
+    let digits = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    digits = digits.map((digit) => (
+      <button type="button" key={digit} id={operators.includes(digit) ? 'operators' : nestedTernary('0'.includes(digit), 'zero', null)}>
+        {digit}
+      </button>
+    ));
     return (
       <div className="calculator">
         <p className="output">0</p>
-        <button id="AC" type="button">
-          AC
-        </button>
-        <button id="+/-" type="button">
-          +/-
-        </button>
-        <button id="%" type="button">
-          %
-        </button>
-        <button id="operators" type="button">
-          ÷
-        </button>
-        <button id="7" type="button">
-          7
-        </button>
-        <button id="8" type="button">
-          8
-        </button>
-        <button id="9" type="button">
-          9
-        </button>
-        <button id="operators" type="button">
-          x
-        </button>
-        <button id="4" type="button">
-          4
-        </button>
-        <button id="5" type="button">
-          5
-        </button>
-        <button id="6" type="button">
-          6
-        </button>
-        <button id="operators" type="button">
-          -
-        </button>
-        <button id="1" type="button">
-          1
-        </button>
-        <button id="2" type="button">
-          2
-        </button>
-        <button id="3" type="button">
-          3
-        </button>
-        <button id="operators" type="button">
-          +
-        </button>
-        <button className="zero" type="button">
-          0
-        </button>
-        <button id="." type="button">
-          .
-        </button>
-        <button id="operators" type="button">
-          =
-        </button>
+        {digits}
       </div>
     );
   }
---
+++ src/logic/helper.js
@@ -0,0 +1,1 @@
+export default (condition, then, otherwise) => (condition ? then : otherwise);
